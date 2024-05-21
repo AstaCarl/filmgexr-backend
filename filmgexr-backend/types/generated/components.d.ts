8365bdf0f4eec31bd1b0cef81907e7df9a2a2fa4
Your Name <asta.carl@akqa.com>
2024-05-21 21:59:36 +0200

    seo plugin opsætning

--- filmgexr-backend/types/generated/components.d.ts
+++ filmgexr-backend/types/generated/components.d.ts
@@ -193,6 +193,57 @@ export interface NavigationLinkNavigationLink extends Schema.Component {
   };
 }
 
+export interface SharedMetaSocial extends Schema.Component {
+  collectionName: 'components_shared_meta_socials';
+  info: {
+    displayName: 'metaSocial';
+    icon: 'project-diagram';
+  };
+  attributes: {
+    socialNetwork: Attribute.Enumeration<['Facebook', 'Twitter']> &
+      Attribute.Required;
+    title: Attribute.String &
+      Attribute.Required &
+      Attribute.SetMinMaxLength<{
+        maxLength: 60;
+      }>;
+    description: Attribute.String &
+      Attribute.Required &
+      Attribute.SetMinMaxLength<{
+        maxLength: 65;
+      }>;
+    image: Attribute.Media;
+  };
+}
+
+export interface SharedSeo extends Schema.Component {
+  collectionName: 'components_shared_seos';
+  info: {
+    displayName: 'seo';
+    icon: 'search';
+  };
+  attributes: {
+    metaTitle: Attribute.String &
+      Attribute.Required &
+      Attribute.SetMinMaxLength<{
+        maxLength: 60;
+      }>;
+    metaDescription: Attribute.String &
+      Attribute.Required &
+      Attribute.SetMinMaxLength<{
+        minLength: 50;
+        maxLength: 160;
+      }>;
+    metaImage: Attribute.Media;
+    metaSocial: Attribute.Component<'shared.meta-social', true>;
+    keywords: Attribute.Text;
+    metaRobots: Attribute.String;
+    structuredData: Attribute.JSON;
+    metaViewport: Attribute.String;
+    canonicalURL: Attribute.String;
+  };
+}
+
 export interface StepsSteps extends Schema.Component {
   collectionName: 'components_steps_steps';
   info: {
@@ -266,6 +317,8 @@ declare module '@strapi/types' {
       'menu.menu': MenuMenu;
       'navigation-link.footer-link': NavigationLinkFooterLink;
       'navigation-link.navigation-link': NavigationLinkNavigationLink;
+      'shared.meta-social': SharedMetaSocial;
+      'shared.seo': SharedSeo;
       'steps.steps': StepsSteps;
       'studio.studios': StudioStudios;
       'team-member.team-member': TeamMemberTeamMember;
